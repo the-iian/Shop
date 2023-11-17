bd438490e7360b83648cf9d12f156fd23b74f7ea
iian <yiaahn@gmail.com>
2023-11-17 15:25:02 +0900

    상품 목록

--- src/main/java/com/jpabook/shop/controller/ItemController.java
+++ src/main/java/com/jpabook/shop/controller/ItemController.java
@@ -1,12 +1,16 @@
 package com.jpabook.shop.controller;
 
 import com.jpabook.shop.domain.item.Book;
+import com.jpabook.shop.domain.item.Item;
 import com.jpabook.shop.service.ItemService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,6 +18,8 @@ public class ItemController {
 
     private final ItemService itemService;
 
+
+    //화면 열기
     @GetMapping("/items/new")
     public String createForm(Model model){
         model.addAttribute("form", new BookForm());
@@ -21,6 +27,8 @@ public class ItemController {
 
     }
 
+
+    //상품 등록
     @PostMapping("/items/new")
     public String create(BookForm form) {
 
@@ -36,4 +44,13 @@ public class ItemController {
 
     }
 
+
+    //상품 목록
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "/items/itemList";
+
+    }
 }
