483d3e622cbcd85f58f964f30387da89e30fffe7
iian <yiaahn@gmail.com>
2023-11-17 14:32:24 +0900

    회원 목록 조회

--- src/main/java/com/jpabook/shop/controller/MemberController.java
+++ src/main/java/com/jpabook/shop/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,6 +24,8 @@ public class MemberController {
         return "members/createMemberForm"; // view로 이동
     }
 
+
+    //회원 등록
     @PostMapping("/members/new") // 데이터를 실제 등록
     public String create(@Valid MemberForm form, BindingResult result) {
         // Valid : MemberForm을 열어보고 검증 어노테이션을 실행, BindingResult : Valid 다음에 있으면 오류가 이곳에 담겨서 아래 코드들이 실행됨
@@ -39,5 +42,14 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+
+    //회원 목록 조회
+    @GetMapping("/members")
+    public String list(Model model) { // model객체를 통해 화면에 데이터를 전달
+        List<Member> members = memberService.findMembers(); // JPQL에서 꺼내서 모든 멤버 조회
+        model.addAttribute("members", members); // 리스트가 꺼내진다
+        return "members/memberList"; // loop를 돌면서 화면에 뿌려짐
     }
 }
